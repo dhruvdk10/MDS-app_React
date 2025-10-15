20b734f0f7c2615487f5b86c0e68794b435df1cf
dhruv <dhruv@example.com>
2025-10-15 22:03:18 +0530

    lu

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "index.css";
+import "./index.css";
 
 // Use HashRouter inside App, no changes needed here
 ReactDOM.createRoot(document.getElementById("root")).render(
