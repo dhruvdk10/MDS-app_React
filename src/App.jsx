d0120e1e5b31d72bbf7d98e10ee5770fcef23ebb
dhruv <dhruv@example.com>
2025-10-15 22:12:39 +0530

    lu

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
+import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import "./index.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
 import Services from "./Pages/Services";
 import ContactUs from "./Pages/ContactUs";
-import Projects from "./Pages/Projects"; 
+import Projects from "./Pages/Projects";
+
 
 const App = () => {
-  useEffect(() => {
+  React.useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
   return (
-    <HashRouter basename="/MDS-app_React/">
+    <BrowserRouter basename="/MDS-app_React/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -25,10 +26,12 @@ const App = () => {
         <Route path="/Services" element={<Services />} />
         <Route path="/ContactUs" element={<ContactUs />} />
         <Route path="/Projects" element={<Projects />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
+
