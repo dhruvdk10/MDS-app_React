286db89f822446de057d360ac77ec2777af28732
dhruv <dhruv@example.com>
2025-10-15 21:59:18 +0530

    lu

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import "./index.css";
+import React, { useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
 import Services from "./Pages/Services";
 import ContactUs from "./Pages/ContactUs";
-import Projects from "./Pages/Projects";
-
+import Projects from "./Pages/Projects"; // if you have a base projects page
+import Residential from "./Pages/Residential";
+import Commercial from "./Pages/Commercial";
 
 const App = () => {
-  React.useEffect(() => {
+  useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
   return (
-    <BrowserRouter basename="/MDS-app_React/">
+    <HashRouter basename="/MDS-app_React/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,12 +27,12 @@ const App = () => {
         <Route path="/Services" element={<Services />} />
         <Route path="/ContactUs" element={<ContactUs />} />
         <Route path="/Projects" element={<Projects />} />
-        {/* <Route path="*" element={<PageNotFound />} /> */}
+        <Route path="/Projects/Residential" element={<Residential />} />
+        <Route path="/Projects/Commercial" element={<Commercial />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
-}
+};
 
 export default App;
-
