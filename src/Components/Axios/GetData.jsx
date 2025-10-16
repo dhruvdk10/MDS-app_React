6fa4a45d8c3b4266834816223361f0738595bf7a
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-16 15:43:39 +0530

    lu

--- src/Components/Axios/GetData.jsx
+++ src/Components/Axios/GetData.jsx
@@ -1,40 +1,40 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+// import axios from 'axios'
+// import React, {useEffect, useState} from 'react'
 
-export const GetData = () => {
-  const [data, setData] = useState([]);
-    useEffect(() => {
-      axios.get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-  return (
-    <>
-    <h2>Axios Data</h2>
-    <ul>
-      {data.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-    </>
-  )
-}
+// export const GetData = () => {
+//   const [data, setData] = useState([]);
+//     useEffect(() => {
+//       axios.get("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => {
+//         setData(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, []);
+//   return (
+//     <>
+//     <h2>Axios Data</h2>
+//     <ul>
+//       {data.map((user) => (
+//         <li key={user.id}>{user.name}</li>
+//       ))}
+//     </ul>
+//     </>
+//   )
+// }
 
-export const PostData = async () => {
-  try {
-    const response = await axios.post("https://jsonplaceholder.typicode.com/posts", {
-      title: "foo",
-      body: "bar",
-      userId: 1,
-    });
-    console.log("Post created:", response.data);
-  } catch (error) {
-    console.error("Error creating post:", error);
-  }
-};
+// export const PostData = async () => {
+//   try {
+//     const response = await axios.post("https://jsonplaceholder.typicode.com/posts", {
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     });
+//     console.log("Post created:", response.data);
+//   } catch (error) {
+//     console.error("Error creating post:", error);
+//   }
+// };
 
-PostData();
+// PostData();
--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
-const DataFetch = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
+// const DataFetch = () => {
+//   const [users, setUsers] = useState([]);
+//   const [loading, setLoading] = useState(false);
 
-  const fetchData = () => {
-    setLoading(true);
-    console.log("Fetching data...");
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        const limitedUsers = data.slice(0, 5);
-        setUsers(limitedUsers);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  };
+//   const fetchData = () => {
+//     setLoading(true);
+//     console.log("Fetching data...");
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const limitedUsers = data.slice(0, 5);
+//         setUsers(limitedUsers);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//         setLoading(false);
+//       });
+//   };
 
-  useEffect(() => {
-  }, []);
+//   useEffect(() => {
+//   }, []);
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <button onClick={fetchData}>Fetch Users Data</button>
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "40px" }}>
+//       <button onClick={fetchData}>Fetch Users Data</button>
 
-      {loading ? (
-        <p>Loading Users Data...</p>
-      ) : (
-        <ul style={{ listStyle: "none" }}>
-          {users.map((user) => (
-            <li key={user.id}> <br />
-              {user.name} <br />
-              {user.email}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
+//       {loading ? (
+//         <p>Loading Users Data...</p>
+//       ) : (
+//         <ul style={{ listStyle: "none" }}>
+//           {users.map((user) => (
+//             <li key={user.id}> <br />
+//               {user.name} <br />
+//               {user.email}
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
 
-export default DataFetch;
+// export default DataFetch;
